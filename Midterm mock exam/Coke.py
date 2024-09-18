9d5fde7a97cf4a52b0791b0ea0dc67d6c40cc07d
Karn <140336765+kxxnD04@users.noreply.github.com>
2024-09-18 19:09:08 +0700

    Update Coke

--- Midterm mock exam/Coke.py
+++ Midterm mock exam/Coke.py
@@ -2,19 +2,12 @@
 def coke(aaa, bbb, ccc, ddd):
     """coke"""
     total = 0
-    if bbb == 0 and ddd != 0:
+    if not bbb:
         total += aaa*ddd
-    elif bbb != 0 and ccc != 0 and ddd != 0:
-        if ddd%bbb != 0:
+    elif ddd:
+        if ddd%bbb:
             total += ((aaa*(ddd-(ddd//bbb)))+(ccc*(ddd//bbb)))
-        elif ddd%bbb == 0:
+        else:
             total += ((aaa*(ddd-((ddd//bbb)-1)))+(ccc*((ddd//bbb)-1)))
-    elif bbb != 0 and ccc == 0 and ddd != 0:
-        if ddd%bbb == 0:
-            total += ((aaa*(ddd-((ddd//bbb)-1)))+(ccc*((ddd//bbb)-1)))
-        elif ddd%bbb != 0:
-            total += ((aaa*(ddd-((ddd//bbb))))+(ccc*((ddd//bbb))))
-    elif ddd == 0:
-        total = 0
     print(total)
 coke(int(input()), int(input()), int(input()), int(input()))
